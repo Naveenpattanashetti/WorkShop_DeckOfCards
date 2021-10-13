b75593ef1c3b0f2485f82b3752eebfaa6b257c33
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-13 09:22:32 +0530

    Added:Program for creating DeckOfCards

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -1,10 +1,52 @@
 package com.bridgelabz.deckofcards;
 
 public class DeckOfCards {
-	
+  /**
+   * Deck of Cards program
+   * */
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome to Deck of cards");
+
+		String[][] cardsCollection = new String[4][13];
+
+		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+		DeckOfCards deckOfCards = new DeckOfCards();
+		String[][] cards = deckOfCards.createCards(suits, ranks, cardsCollection);
+		deckOfCards.showCards(cards);
 	}
+
+	/**
+	 * Create all cards
+	 * */
+	public String[][] createCards(String[] suit, String[] ranks, String[][] cardsCollection) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 13; j++) {
+
+				cardsCollection[i][j] = (suit[i] + "-" + ranks[j]);
+			}
+
+		}
+
+		return cardsCollection;
+
+	}
+	
+	/**
+	 * Display all cards
+	 * */
+	public void showCards(String[][] cards){
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 13; j++) {
+
+				 System.out.println(cards[i][j]+"");
+			}
+			
+
+		}
+		
+	}
+	
 
 }
